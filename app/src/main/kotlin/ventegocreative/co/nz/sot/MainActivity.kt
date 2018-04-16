2e2cb2d805f8845b1fca47e41fa552573831361e
Kai Koenig <kai@ventego-creative.co.nz>
2018-04-16 13:51:46 +1200

    Step 5 - Butterknife

--- app/src/main/kotlin/ventegocreative/co/nz/sot/MainActivity.kt
+++ app/src/main/kotlin/ventegocreative/co/nz/sot/MainActivity.kt
@@ -3,25 +3,27 @@ package ventegocreative.co.nz.sot
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import kotlinx.android.synthetic.main.activity_main.*
-
+import android.widget.TextView
+import butterknife.BindView
+import butterknife.ButterKnife
+import butterknife.OnClick
 
 class MainActivity : AppCompatActivity() {
+
+    @BindView(R.id.helloWorld)
+    lateinit var helloWorld: TextView
+
+    @OnClick(R.id.clickMe)
+    fun clickedClickMe() {
+        Log.d("MainActivity", "onClick: clickMe")
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        helloWorld.setText("Hello via KAE")
+        ButterKnife.bind(this)
+        helloWorld.text = "Hello via Butterknife"
 
-        // plain Kotlin:
-        //
-        // 1. findViewById(R.id.clickMe).setOnClickListener { view -> Log.d("MainActivity", "onClick: clickMe") }
-        // or
-        // 2. findViewById(R.id.clickMe).setOnClickListener({ view -> Log.d("MainActivity", "onClick: clickMe")})
-
-        // with Kotlin Android Extensions:
-        //
-        clickMe.setOnClickListener { Log.d("MainActivity", "onClick: clickMe") }
-    }
+  }
 }
