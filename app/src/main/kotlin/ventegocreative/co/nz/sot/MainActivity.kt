b866c438cb9df57a2e4fc2ec664087048297a202
Kai Koenig <kai@ventego-creative.co.nz>
2018-04-16 13:45:42 +1200

    Step 4 - Click Handler

--- app/src/main/kotlin/ventegocreative/co/nz/sot/MainActivity.kt
+++ app/src/main/kotlin/ventegocreative/co/nz/sot/MainActivity.kt
@@ -2,6 +2,7 @@ package ventegocreative.co.nz.sot
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -12,5 +13,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         helloWorld.setText("Hello via KAE")
+
+        // plain Kotlin:
+        //
+        // 1. findViewById(R.id.clickMe).setOnClickListener { view -> Log.d("MainActivity", "onClick: clickMe") }
+        // or
+        // 2. findViewById(R.id.clickMe).setOnClickListener({ view -> Log.d("MainActivity", "onClick: clickMe")})
+
+        // with Kotlin Android Extensions:
+        //
+        clickMe.setOnClickListener { Log.d("MainActivity", "onClick: clickMe") }
     }
 }
