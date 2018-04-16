03e65c1c56990928f59abc980ae00c60a5681a7b
Kai Koenig <kai@ventego-creative.co.nz>
2018-04-16 13:44:13 +1200

    Step 3 - Kotlin Android Extensions

--- app/src/main/kotlin/ventegocreative/co/nz/sot/MainActivity.kt
+++ app/src/main/kotlin/ventegocreative/co/nz/sot/MainActivity.kt
@@ -2,11 +2,15 @@ package ventegocreative.co.nz.sot
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        helloWorld.setText("Hello via KAE")
     }
 }
