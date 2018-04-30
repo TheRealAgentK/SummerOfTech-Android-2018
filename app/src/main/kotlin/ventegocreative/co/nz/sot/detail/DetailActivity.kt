fed4dca95f9a99b146703f11d70569ec724e0c44
Kate Henderson <kate@paperkite.co.nz>
2018-04-30 15:27:55 +1200

    Step 20 - Add detail view model using databinding, use RoboHash API for images

--- app/src/main/kotlin/ventegocreative/co/nz/sot/detail/DetailActivity.kt
+++ app/src/main/kotlin/ventegocreative/co/nz/sot/detail/DetailActivity.kt
@@ -2,25 +2,30 @@ package ventegocreative.co.nz.sot.detail
 
 import android.content.Context
 import android.content.Intent
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
 import ventegocreative.co.nz.sot.R
 import ventegocreative.co.nz.sot.data.models.Film
+import ventegocreative.co.nz.sot.databinding.ActivityDetailBinding
 
 
 class DetailActivity : AppCompatActivity() {
+	
+	lateinit var binding: ActivityDetailBinding
 	
 	lateinit var film: Film
 	
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_detail)
+		binding = DataBindingUtil.setContentView(this, R.layout.activity_detail)
 		
 		supportActionBar?.setDisplayHomeAsUpEnabled(true)
 		
 		film = intent.getParcelableExtra(EXTRA_FILM)
 		
+		binding.model = DetailViewModel(film)
 		
 	}
 	
