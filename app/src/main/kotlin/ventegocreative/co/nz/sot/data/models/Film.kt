68cb981838ba2ac5217dbea5db3e6402762d5d70
Kate Henderson <kate@paperkite.co.nz>
2018-05-22 15:22:02 +1200

    Step AC 1: Add Room dependencies, convert Film to a Room Enitity

--- app/src/main/kotlin/ventegocreative/co/nz/sot/data/models/Film.kt
+++ app/src/main/kotlin/ventegocreative/co/nz/sot/data/models/Film.kt
@@ -1,29 +1,43 @@
 package ventegocreative.co.nz.sot.data.models
 
+import android.arch.persistence.room.ColumnInfo
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
 import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 import kotlinx.android.parcel.Parcelize
 import java.util.*
 
 @Parcelize
+@Entity(tableName = Film.TABLE_NAME)
 data class Film(
-		@SerializedName("id") val id: String,
-		@SerializedName("title") val title: String,
-		@SerializedName("description") val description: String,
-		@SerializedName("director") val director: String,
-		@SerializedName("release_date") val releaseDate: String
+		@PrimaryKey
+		@ColumnInfo(name = "id")
+		@SerializedName("id")
+		val id: String,
 		
-//		Everything below is included in the API, but not needed for this basic app
-//		@SerializedName("producer") val producer: String,
-//		@SerializedName("rt_score") val rtScore: String,
-//		@SerializedName("people") val people: List<String>,
-//		@SerializedName("species") val species: List<String>,
-//		@SerializedName("locations") val locations: List<String>,
-//		@SerializedName("vehicles") val vehicles: List<String>,
-//		@SerializedName("url") val url: String
+		@ColumnInfo(name = "title")
+		@SerializedName("title")
+		val title: String,
+		
+		@ColumnInfo(name = "description")
+		@SerializedName("description")
+		val description: String,
+		
+		@ColumnInfo(name = "director")
+		@SerializedName("director")
+		val director: String,
+		
+		@ColumnInfo(name = "release_date")
+		@SerializedName("release_date")
+		val releaseDate: String
+		
 ) : Parcelable {
 	
 	constructor(title: String, description: String, director: String, releaseDate: String) :
 			this(UUID.randomUUID().toString(), title, description, director, releaseDate)
 	
+	companion object {
+		const val TABLE_NAME = "films"
+	}
 }
